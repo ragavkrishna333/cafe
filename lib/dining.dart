49524cc16ca1f8fbd1bdc8365ff2726780a5dcc5
Ragavkrishna <ragavkrishna@fcsonline.co.in>
2024-03-12 10:28:19 +0530

    update selection

--- lib/dining.dart
+++ lib/dining.dart
@@ -36,6 +36,7 @@ class _DiningState extends State<Dining> {
               ),
             ),
             floating: true,
+            pinned: true,
             expandedHeight: 200,
           ),
           SliverList(
@@ -44,53 +45,82 @@ class _DiningState extends State<Dining> {
             return Container(
                 color: Color.fromARGB(118, 161, 113, 96),
                 height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: AssetImage(dish[index]["image"]),
-                              fit: BoxFit.cover)),
+                child: ListTile(
+                  onTap: () {
+                    setState(() {
+                      if (isSelected) {
+                        dish[index]["add"] = !dish[index]["add"];
+                      }
+                    });
+                  },
+                  onLongPress: () {
+                    setState(() {
+                      dish[index]["add"] = true;
+                      isSelected = true;
+                    });
+                  },
+                  selected: dish[index]["add"] ?? false,
+                  visualDensity: VisualDensity(vertical: 0.5),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  contentPadding: EdgeInsets.zero,
+                  leading: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(
+                      dish[index]["image"],
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(40, 30, 0, 0),
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Color.fromARGB(255, 133, 94, 78),
-                                  borderRadius: BorderRadius.circular(20)),
-                              child: Text(
-                                dish[index]["title"],
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: "Inter",
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ),
+                  ),
+
+                  // Container(
+                  //   margin: EdgeInsets.only(top: 20),
+                  //   height: 100,
+                  //   width: 100,
+                  //   decoration: BoxDecoration(
+                  //       shape: BoxShape.circle,
+                  //       image: DecorationImage(
+                  //           image: AssetImage(dish[index]["image"]),
+                  //           fit: BoxFit.cover)),
+                  // ),
+                  title: Padding(
+                    padding: EdgeInsets.only(right: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 133, 94, 78),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Text(
+                            dish[index]["title"],
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontFamily: "Inter",
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600),
                           ),
-                          Align(
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                "₹ ${dish[index]["price"]}",
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w600),
-                              ))
-                        ],
-                      ),
-                    )
-                  ],
+                        ),
+                        Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "₹ ${dish[index]["price"]}",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w600),
+                            ))
+                      ],
+                    ),
+                  ),
+                  trailing: Visibility(
+                      visible: isSelected,
+                      child: Checkbox(
+                        value: dish[index]["add"] ?? false,
+                        onChanged: (value) {
+                          setState(() {
+                            currentIndex = index;
+                            dish[index]["add"] = value!;
+                            // isSelected = value!;
+                          });
+                        },
+                      )),
                 ));
           })),
         ],
@@ -111,9 +141,55 @@ class _DiningState extends State<Dining> {
                       offset: Offset(0, 0))
                 ],
                 borderRadius: BorderRadius.circular(20),
-                color: const Color.fromARGB(255, 63, 44, 38)),
+                color: Color.fromRGBO(63, 44, 38, 1)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Color.fromRGBO(100, 70, 61, 1),
+                    onPressed: () {},
+                    child: Text(
+                      "Add",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: isSelected,
+                  child: MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        isSelected = false;
+                        cancel();
+                        // dish[currentIndex!]["add"];
+                      });
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ))
     ]));
+  }
+}
+
+cancel() {
+  for (var element in dish) {
+    element["add"] = false;
   }
 }
 
@@ -121,17 +197,39 @@ List<Map> dish = [
   {
     "image": "assets/images/cococoffee.jpg",
     "title": "Coco Coffee",
-    "price": 120
+    "price": 120,
+    "add": false
   },
   {
     "image": "assets/images/FCoffee.jpeg",
     "title": "Filter coffee",
-    "price": 100
+    "price": 100,
+    "add": false
   },
-  {"image": "assets/images/amaricano.jpg", "title": "Americano", "price": 110},
-  {"image": "assets/images/mocha.jpg", "title": "Mocha", "price": 260},
-  {"image": "assets/images/Espresso.jpeg", "title": "Espresso", "price": 90},
-  {"image": "assets/images/latte.jpeg", "title": "Latte", "price": 160},
+  {
+    "image": "assets/images/amaricano.jpg",
+    "title": "Americano",
+    "price": 110,
+    "add": false
+  },
+  {
+    "image": "assets/images/mocha.jpg",
+    "title": "Mocha",
+    "price": 260,
+    "add": false
+  },
+  {
+    "image": "assets/images/Espresso.jpeg",
+    "title": "Espresso",
+    "price": 90,
+    "add": false
+  },
+  {
+    "image": "assets/images/latte.jpeg",
+    "title": "Latte",
+    "price": 160,
+    "add": false
+  },
   {
     "image": "assets/images/capaccino.jpg",
     "title": "Cappucciono",
@@ -140,11 +238,15 @@ List<Map> dish = [
   {
     "image": "assets/images/coldcoffee.png",
     "title": "Cold Coffee",
-    "price": 150
+    "price": 150,
+    "add": false
   },
   {
     "image": "assets/images/hotchoco.jpg",
     "title": "Hot Chocolate",
-    "price": 190
+    "price": 190,
+    "add": false
   }
 ];
+bool isSelected = false;
+int? currentIndex;
